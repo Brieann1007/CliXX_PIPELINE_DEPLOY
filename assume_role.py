4843235bfa10fe03eda5ee08fb10547f8626976a
Brieann1007 <brieann1007@gmail.com>
2024-11-02 19:26:35 -0700

    Yea hoe >;)

--- assume_role.py
+++ assume_role.py
@@ -442,7 +442,7 @@ def create_load_balancer(security_group_id,tg_arn, public_subnets):
     save_to_ssm('/clixx/LoadbalancerDNS', load_balancer_dns)
     return load_balancer_arn, load_balancer_dns   
 
-def create_rds_instance(vpc_id, private_subnets, security_group_id):
+def create_rds_instance(private_subnets, security_group_id):
     sts_client=boto3.client('sts')
     #Calling the assume_role function
     assumed_role_object=sts_client.assume_role(RoleArn='arn:aws:iam::054037131148:role/Engineer', RoleSessionName='mysession')
@@ -632,6 +632,6 @@ if __name__=="__main__":
     file_system_id = create_file_system()
     tg_arn = create_target_group(vpc_id)
     load_balancer_arn, load_balancer_dns= create_load_balancer(security_group_id,tg_arn, public_subnets)
-    rds_identifier, rds_endpoint_address = create_rds_instance(vpc_id, private_subnets, security_group_id)
+    rds_identifier, rds_endpoint_address = create_rds_instance(private_subnets, security_group_id)
     asg_arn = create_autoscaling_group(security_group_id, tg_arn, public_subnets)
     
